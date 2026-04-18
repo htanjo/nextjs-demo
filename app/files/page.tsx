767903aa872c8c41e3b71c888c0e1a43c54959d0
Hiroyuki Tanjo <expheno@gmail.com>
2026-04-18 21:58:15 +0900

    Add files page

---
+++ app/files/page.tsx
@@ -0,0 +1,10 @@
+import { Container } from "@mui/material";
+
+export default function Files() {
+  return (
+    <Container>
+      <h1>Files</h1>
+      <p>This is the files page.</p>
+    </Container>
+  );
+}
