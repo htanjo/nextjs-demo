0c5f4ae92abf86154ed9d5127facb1654659c662
Hiroyuki Tanjo <expheno@gmail.com>
2026-04-18 23:47:16 +0900

    Add back to home button on notes page

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,10 +1,13 @@
-import { Container } from "@mui/material";
+import { Button, Container } from "@mui/material";
 
 export default function Notes() {
   return (
     <Container>
       <h1>Notes</h1>
       <p>This is the notes page.</p>
+      <Button variant="contained" href="/">
+        Back to Home
+      </Button>
     </Container>
   );
 }
