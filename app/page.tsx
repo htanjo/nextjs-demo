0e9eaa9eff0660288882aea0f4f2a3b9a6e88675
Hiroyuki Tanjo <expheno@gmail.com>
2026-04-18 21:52:10 +0900

    Refactor page layout and replace links with Material UI Button component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { Button } from "@mui/material";
 
 export default function Home() {
   return (
@@ -36,29 +37,9 @@ export default function Home() {
           </p>
         </div>
         <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className={styles.secondary}
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+          <Button variant="contained" href="/files">
+            Files
+          </Button>
         </div>
       </main>
     </div>
